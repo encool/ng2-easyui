2bc8582018d6305cfc7d3cc309870ef63034bff1
qiao.li <qiao.li@chiancreator.com>
2017-10-09 17:16:59 +0800

    bpmn

--- src/core/bpmn/bpmn.service.ts
+++ src/core/bpmn/bpmn.service.ts
@@ -1,20 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http, URLSearchParams, RequestOptions, Response } from '@angular/http';
 
-import { Observable } from 'rxjs/Observable';
-
-
 export abstract class EuBpmnService {
 
     constructor(public http: Http) { }
 
-    abstract getPage(url: string, rows: number, page: number,
-        cond?: Object, sidx?: string, sord?: string, _search?: boolean): Promise<any>
+    abstract getModelInfo(modelId: string): Promise<{}>
 
-    abstract getPageOb(url: string, pageSize: number, page: number,
-        cond?: Object, sidx?: string, sord?: string, _search?: boolean): Observable<any>
+    abstract saveModel(modelId: string, xml: string, svg?: string): Promise<any>
 
-    abstract getPageObByOffsetLimit(url: string, offset: number, limit: number,
-        cond?: Object, sidx?: string, sord?: string, _search?: boolean): Observable<any>
+    abstract getProcessDefDiagramXml(modelId: string): Promise<any>
 
 }
